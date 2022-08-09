e10fbf92451199af94e91624a4a3cb87d6efe217
jikwan <jikwan0327@gmail.com>
2022-08-08 21:48:03 -0700

    build

--- chocokcake/src/components/Menu/menu.js
+++ chocokcake/src/components/Menu/menu.js
@@ -4,29 +4,29 @@ import menuimg from "../../img/Icon/Menu.png";
 import MenuItems from "./menuItems";
 
 function Menu({ accountId }) {
-    const [clicked, setClicked] = useState(false);
-    const [accountId1, setAccountId1] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [accountId1, setAccountId1] = useState("");
 
-    return (
-        <>
-            <MenuButton onClick={() => setClicked(true)}>
-                <img src={menuimg} />
-            </MenuButton>
-            <MenuItems accountId={accountId} clicked={clicked} setClicked={setClicked} />
-        </>
-    );
+  return (
+    <>
+      <MenuButton onClick={() => setClicked(true)}>
+        <img src={menuimg} />
+      </MenuButton>
+      <MenuItems accountId={accountId} clicked={clicked} setClicked={setClicked} />
+    </>
+  );
 }
 
 const MenuButton = styled.button`
-    position: absolute;
-    top: 27px;
-    right: 54px;
-    border: none;
-    background-color: #fff6ea;
-    > img {
-        width: 45px;
-        height: 45px;
-    }
+  position: absolute;
+  top: 27px;
+  right: 54px;
+  border: none;
+  background-color: #fff6ea;
+  > img {
+    width: 45px;
+    height: 45px;
+  }
 `;
 
 export default Menu;
